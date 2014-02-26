21cf34c420855c8a32b636bdf10c082fc29724e6
thysok <thysok@gmail.com>
2014-02-26 15:04:59 -0800

    test

---
+++ ThySok/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThySok.Startup))]
+namespace ThySok
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
